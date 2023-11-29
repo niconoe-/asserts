f03c8bf756b1ac2ae7b46027843dd5c590512a7a
Nicolas Giraud <nicolas.giraud@aareon.com>
2023-11-29 16:21:27 +0100

    Add `assertJsonValidate` method.

--- src/Asserts/Categories/AssertJsonTrait.php
+++ src/Asserts/Categories/AssertJsonTrait.php
@@ -4,13 +4,22 @@ declare(strict_types=1);
 namespace Nicodev\Asserts\Categories;
 
 use Throwable;
+
+use function json_decode;
 use function json_last_error;
+use function json_validate;
 use const JSON_ERROR_NONE;
 
 /**
  * Trait AssertJsonTrait
  *
  * List of assertions associated to JSON.
+ *
+ * TODO:
+ * When traits can have const (PHP 8.2), this trait will define:
+ * - private int const ASSERT_JSON_RETURN_NULL = 0;
+ * - private int const ASSERT_JSON_RETURN_ARRAY = 1;
+ * - private int const ASSERT_JSON_RETURN_OBJECT = 2;
  */
 trait AssertJsonTrait
 {
@@ -24,5 +33,23 @@ trait AssertJsonTrait
     {
         self::makeAssertion(JSON_ERROR_NONE === json_last_error(), $exception);
         return true;
+    }
+
+    /**
+     * Asserts that the given string is JSON encoded, and possibly returns the decoded version into array or object.
+     *
+     * @param string $string To validate if JSON string
+     * @param callable(): Throwable $exception The exception to throw if the assertion fails.
+     * @param int $returnMode "0" means no decode. "1" means decode in associative way. "2" means decode in object way.
+     * @return mixed
+     */
+    protected static function assertJsonValidate(string $string, callable $exception, int $returnMode = 0): mixed
+    {
+        self::makeAssertion(json_validate($string), $exception);
+        if (0 === $returnMode) {
+            return null;
+        }
+        /** @noinspection JsonEncodingApiUsageInspection Ignore taking advantage of throwing error as JSON is valid. */
+        return json_decode($string, 1 === $returnMode);
     }
 }
---
+++ src/Tests/Asserts/ViaParentClass/TraitJson/JsonValidateTest.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+
+namespace Nicodev\Tests\Asserts\ViaParentClass\TraitJson;
+
+use Exception;
+use Nicodev\Tests\Resources\ParentClass;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Final class JsonValidateTest
+ */
+final class JsonValidateTest extends TestCase
+{
+    private readonly object $testClass;
+
+    protected function setUp(): void
+    {
+        $this->testClass = new class() extends ParentClass
+        {
+            /**
+             * Run the assertion is ok for test, but expects no return.
+             * @return mixed
+             */
+            public function runOkNoReturn(): mixed
+            {
+                return self::assertJsonValidate('{"foo": "bar"}', $this->error);
+            }
+
+            /**
+             * Run the assertion is ok for test, but expects associative array.
+             * @return mixed
+             */
+            public function runOkAssoc(): mixed
+            {
+                return self::assertJsonValidate('{"foo": "bar"}', $this->error, 1);
+            }
+
+            /**
+             * Run the assertion is ok for test, but expects object.
+             * @return mixed
+             */
+            public function runOkObject(): mixed
+            {
+                return self::assertJsonValidate('{"foo": "bar"}', $this->error, 2);
+            }
+
+            /**
+             * Run the assertion is KO for test.
+             * @return bool
+             */
+            public function runKo(): bool
+            {
+                return self::assertJsonValidate('{"', $this->error);
+            }
+        };
+    }
+
+    public function testMakeAssertionOKNoReturn(): void
+    {
+        self::assertNull($this->testClass->runOkNoReturn());
+    }
+
+    public function testMakeAssertionOKAssoc(): void
+    {
+        self::assertSame(['foo' => 'bar'], $this->testClass->runOkAssoc());
+    }
+
+    public function testMakeAssertionOKObject(): void
+    {
+        self::assertEquals((object)['foo' => 'bar'], $this->testClass->runOkObject());
+    }
+
+    public function testMakeAssertionKO(): void
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('This assertion fails.');
+        $this->testClass->runKo();
+    }
+}
